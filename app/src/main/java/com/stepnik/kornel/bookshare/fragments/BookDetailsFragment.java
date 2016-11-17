1a63f94faa3757f3f3f5ab68535d83d4ad6862c6
korSt <k4z1kpl@gmail.com>
2016-11-17 22:46:17 +0100

    Fixed bookdetails callback

--- app/src/main/java/com/stepnik/kornel/bookshare/fragments/BookDetailsFragment.java
+++ app/src/main/java/com/stepnik/kornel/bookshare/fragments/BookDetailsFragment.java
@@ -7,12 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.stepnik.kornel.bookshare.MainActivity;
 import com.stepnik.kornel.bookshare.R;
 import com.stepnik.kornel.bookshare.models.Book;
 import com.stepnik.kornel.bookshare.models.Data;
+
+import retrofit2.http.POST;
 
 /**
  * Created by korSt on 03.11.2016.
@@ -23,10 +26,19 @@ public class BookDetailsFragment extends Fragment {
     public static final String ARG_BOOK = "book";
     int mCurrentPosition = -1;
     Book selectedBook;
+    OnBookSelectedListener mCallback;
+
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+
+        try {
+            mCallback = (OnBookSelectedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
     }
 
     @Nullable
@@ -34,15 +46,25 @@ public class BookDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_book_details, container, false);
         TextView tempTextView = (TextView) rootView.findViewById(R.id.temp_text_mybooks);
+        Button rentBook = (Button) rootView.findViewById(R.id.b_rentbook);
         tempTextView.setText("My books");
 
         if (savedInstanceState != null) {
 //            mCurrentPosition = savedInstanceState.getInt(ARG_POSITION);
             selectedBook = (Book) savedInstanceState.getSerializable(ARG_BOOK);
+        } else {
+            selectedBook = (Book) getArguments().getSerializable(ARG_BOOK);
         }
 
         MainActivity mainActivity = (MainActivity) getContext();
         mainActivity.setTitle("Book detals");
+
+        rentBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCallback.onRentBookSelected(selectedBook);
+            }
+        });
 
         return rootView;
     }
@@ -50,6 +72,7 @@ public class BookDetailsFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
     }
 
     @Override
